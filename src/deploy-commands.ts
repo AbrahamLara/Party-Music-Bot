9b62a2e6e05192d6d1989dd905695f4678bb7ee1
= <=>
2021-10-09 21:34:01 -0400

    chore(bot): Implemented basic bot functionality for connecting.
    Closes https://github.com/AbrahamLara/Party-Music-Bot/issues/4

---
+++ src/deploy-commands.ts
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
+import path from 'path';
+import { BOT_TOKEN, CLIENT_ID, GUILD_ID } from '../config.json';
+
+// An array of available bot commands.
+export const commands: any[] = [];
+const commandFiles = fs.readdirSync(path.join(__dirname, '/commands'));
+
+for (const file of commandFiles) {
+  const command = require(path.join(__dirname, `/commands/${file}`)).default;
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(BOT_TOKEN);
+// Using the discord REST API lets set the commands the bot should respond to. This should only be done when adding
+// or removing commands.
+rest
+  .put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands })
+  .then(() => console.log('Successfully registered application commands.'))
+  .catch(console.error);
